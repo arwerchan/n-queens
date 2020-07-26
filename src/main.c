fb5c117fc86876ff2a0919ad076121b288b61a72
Andrew Werchan <arwerchan@gmail.com>
2020-07-25 22:39:24 -0700

    initial project structure created

---
+++ src/main.c
@@ -0,0 +1,9 @@
+/**
+ * @author      : Andrew Werchan (arwerchan@gmail.com)
+ * @file        : main
+ * @created     : Saturday Jul 25, 2020 22:37:40 MST
+ */
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
